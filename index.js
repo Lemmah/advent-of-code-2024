94b6d62f2991b0f517cc1263231426351a1fa509
Lemayian Nakolah <jnlemayian@gmail.com>
2024-12-01 22:34:11 +0300

    feat: parse input file

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 //@ts-check
+const fs = require('node:fs/promises');
 
 /**
  * Calculate the distance apart between to provided lists
  * 
  * @param {number[]} left - first group list
  * @param {number[]} right - second group list
- * @returns {number}
+ * 
+ * @returns {number} - distance apart
  */
 const getDistanceApart = (left, right) => {
   let total = 0;
@@ -17,6 +19,28 @@ const getDistanceApart = (left, right) => {
   return total;
 }
 
+/**
+ * Parse input file to left and right list
+ * 
+ * @param {string} inputFilePath
+ * 
+ * @returns {Promise<[number[], number[]]>} - left[], right[]
+ */
+async function parseInputFile(inputFilePath) {
+  let left = [];
+  let right = [];
+
+  const file = await fs.open(inputFilePath, 'r');
+  for await (const line of file.readLines()) {
+    const [leftLocationId, rightLocationId] = line.split('   ');
+    left.push(Number(leftLocationId));
+    right.push(Number(rightLocationId));
+  }
+
+  return [left, right];
+}
+
 module.exports = {
-  getDistanceApart
+  getDistanceApart,
+  parseInputFile
 }
