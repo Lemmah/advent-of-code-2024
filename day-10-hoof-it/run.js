9922ef5f9893e3e9e1f34b6f420dcf0d99fccfe5
Lemayian Nakolah <jnlemayian@gmail.com>
2024-12-10 23:52:28 +0300

    chore: run against aoc input

---
+++ day-10-hoof-it/run.js
@@ -0,0 +1,9 @@
+const { getTotalTrailheadsScores } = require('./index.js');
+const fs = require('node:fs/promises');
+
+(async (inputFile) => {
+  const data = await fs.readFile(inputFile, { encoding: 'utf-8' });
+  const topographicMap = data.split('\n').map(row => row.split('').map(pos => Number(pos)));
+
+  console.log('#1, Total Trailheads Scores:', getTotalTrailheadsScores(topographicMap));
+})('input.txt');
